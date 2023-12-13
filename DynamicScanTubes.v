ba6f69e72862098299a5685c4ae93bd36bcdebaf
narozeol <mashironaro@gmail.com>
2023-12-14 02:20:48 +0800

    将动态扫描数码管的代码移动至外层文件夹（暂时不知道如何分文件夹编译）

--- DynamicScanTubes.v
+++ DynamicScanTubes.v
@@ -4,8 +4,8 @@ module DynamicScanTubes(clk_50M, DataIn, DIG, codeout);
     input [9:0] DataIn;//计数器的值
     
     //输出信号
-    output [7:0] DIG;
-    output [6:0] codeout;
+    output reg[7:0] DIG;
+    output reg[6:0] codeout;
 
     //内部信号
 	wire [1:0] SEL;
@@ -13,7 +13,7 @@ module DynamicScanTubes(clk_50M, DataIn, DIG, codeout);
 	wire clk_1k;
 	
 	DivFreq e(clk_50M, clk_1k);//分频器	
-    ModFourCounter a(clk_1k, en, SEL);//模4计数器
+    ModFourCounter a(clk_1k, SEL);//模4计数器
     DataTransformer b(SEL, DataIn, Y);//4-1数据选择器(选择计数器输入的某一位)
     Decoder c(Y, codeout);///七段线译码器
     Two2FourDecoder d(SEL, DIG);//2-4译码器，选择要亮起的数码管
